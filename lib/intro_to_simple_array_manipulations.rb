517b7ff26add72426ae588dd4fa908f32ed2bc50
Emmanuel Jose <emjose@gmail.com>
2020-07-15 23:48:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,16 +11,13 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  array = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
   array.pop(2)
 end
 
 def using_shift(array)
-  array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   array.shift
 end
 
 def shift_with_args(array)
-  array = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   array.shift(2)
 end
