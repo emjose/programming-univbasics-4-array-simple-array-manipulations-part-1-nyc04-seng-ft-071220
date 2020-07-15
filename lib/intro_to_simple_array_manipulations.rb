41bdf2bb376842524443505de4461d1dfcc1efe0
Emmanuel Jose <emjose@gmail.com>
2020-07-15 23:47:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,17 +1,12 @@
 def using_push(array, string)
-  array = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  string = "violet"
   array.push(string)
 end
 
 def using_unshift(array, string)
-  array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  string = "Staten Island"
-  array.unshift("Staten Island")
+  array.unshift(string)
 end
 
 def using_pop(array)
-  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   array.pop
 end
 
