1ac0b8ca8b3e6ec91efa7a41e4083f63567feae7
Emmanuel Jose <emjose@gmail.com>
2020-07-15 22:45:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,5 +12,6 @@ end
 
 def using_pop(array)
   array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  array.pop
+  new_array = array.pop(1)
+new_array
 end
