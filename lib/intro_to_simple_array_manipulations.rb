efd69838f9c8b143ea151b3ef1ce5130d1ca5afd
Emmanuel Jose <emjose@gmail.com>
2020-07-15 22:56:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,17 +12,17 @@ end
 
 def using_pop(array)
   array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  array.pop
+  return array.pop
 end
 
 def pop_with_args(array)
   array = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  array.pop(2)
+  return array.pop(2)
 end
 
 def using_shift(array)
   array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  array.shift
+  return array.shift
 end
 
 def shift_with_args(array)
